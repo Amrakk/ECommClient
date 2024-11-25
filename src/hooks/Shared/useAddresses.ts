9f340d4e6157208ab7293b48c26bc1fc79db0a72
Amrakk <4mr4kk@gmail.com>
2024-11-25 17:49:55 +0700

    feat: implement crawl addresses notify

--- src/hooks/Shared/useAddresses.ts
+++ src/hooks/Shared/useAddresses.ts
@@ -1,13 +1,20 @@
 import { useEffect } from "react";
+import { toast } from "react-toastify";
 import { useQuery } from "@tanstack/react-query";
+import { useUserStore } from "@/stores/user.store";
+import ToastInitContent from "@/components/Shared/ToastInitContent";
 import { useAddressesStore } from "@/stores/addresses.store";
 import { getWards, getDistricts, getProvinces } from "@/apis/services";
 
 export default function useAddresses() {
+    const user = useUserStore((state) => state.user);
     const setWards = useAddressesStore((state) => state.setWards);
     const setDistricts = useAddressesStore((state) => state.setDistricts);
     const setProvinces = useAddressesStore((state) => state.setProvinces);
 
+    // TODO: somehow notify for the admin that the addresses is not crawled yet,
+    // and they need to crawl it first before using it in the advanced panel. Try to use a toast or a modal.
+    // Tell the user SERVICE_UNAVAILABLE
     const assignAddresses = useQuery({
         queryKey: ["addresses"],
         queryFn: async () => {
@@ -22,6 +29,28 @@ export default function useAddresses() {
         setDistricts(assignAddresses.data[1]);
         setWards(assignAddresses.data[2]);
     }, [assignAddresses.data]);
+
+    useEffect(() => {
+        if (assignAddresses.error && user && user.role === "admin") {
+            toast.info(ToastInitContent, {
+                position: "top-center",
+                theme: "colored",
+                toastId: "crawl-addresses-notify",
+                style: {
+                    width: "700px",
+                    translate: "-150px",
+                },
+                closeButton: false,
+                bodyClassName: () => "text-white select-none text-xl font-bold p-4 flex flex-col gap-4 w-[700px]",
+                autoClose: false,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+    }, [assignAddresses.error]);
 
     return { assignAddresses };
 }
